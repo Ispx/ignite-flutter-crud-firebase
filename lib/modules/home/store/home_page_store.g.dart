48612a763c0d5effd07429f9aed60cfdb1cc2232
Isaque Paixao <isaquespx98@gmail.com>
2022-03-21 07:59:41 -0300

    computed  status from toDoListStream

--- lib/modules/home/store/home_page_store.g.dart
+++ lib/modules/home/store/home_page_store.g.dart
@@ -9,6 +9,13 @@ part of 'home_page_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$HomePageStore on _HomePageStore, Store {
+  Computed<StreamStatus>? _$statusComputed;
+
+  @override
+  StreamStatus get status =>
+      (_$statusComputed ??= Computed<StreamStatus>(() => super.status,
+              name: '_HomePageStore.status'))
+          .value;
   Computed<List<ToDoItem>>? _$itemListComputed;
 
   @override
@@ -43,6 +50,7 @@ mixin _$HomePageStore on _HomePageStore, Store {
   String toString() {
     return '''
 toDoListStream: ${toDoListStream},
+status: ${status},
 itemList: ${itemList},
 undoneTaskListLength: ${undoneTaskListLength}
     ''';
